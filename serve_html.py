172985821bd38e2d0b812f1b908391eee26b7860
Naveen Singh <sngh.naveen@gmail.com>
2025-11-25 22:35:02 +0530

    resuse port

--- serve_html.py
+++ serve_html.py
@@ -3,8 +3,11 @@ import socketserver
 
 PORT = 8000
 
+class ReusableTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with ReusableTCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
